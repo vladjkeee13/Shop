d8eed00dc03e3a89611d89dcee7a87de658796e3
vladjkeee13 <vladjkeee13@ukr.net>
2019-01-28 16:29:18 +0200

    Second step(urls,view,templates)

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core.views import HomeView, JackWolfsinViews, TheNorthFaceView, BerghausView
+
+urlpatterns = [
+    path('', HomeView.as_view()),
+    path('jack-wolfskin/', JackWolfsinViews.as_view(), name='jack-wolfskin'),
+    path('the-north-face/', TheNorthFaceView.as_view(), name='the-north-face'),
+    path('berghaus/', BerghausView.as_view(), name='berghaus')
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'homePage.html'
+
+
+class JackWolfsinViews(TemplateView):
+    template_name = 'Brands/JackWolfskin.html'
+
+
+class TheNorthFaceView(TemplateView):
+    template_name = 'Brands/TheNorthFace.html'
+
+#
+# class BerghausView(TemplateView):
+#     template_name = 'Brands/Berghaus.html'
+
+
+class BerghausView(View):
+
+    def get(self, request):
+        return render(request, 'Brands/Berghaus.html')
