017948ca552f22cd2739ddc5e3dc4139f206db61
vladjkeee13 <vladjkeee13@ukr.net>
2019-02-11 14:30:04 +0200

    model 'Product' was registered to admin panel. Also method 'str' was added to model 'Product' and model 'Feedback' was migrated

--- product/models.py
+++ product/models.py
@@ -13,6 +13,9 @@ class Product(models.Model):
     size = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
 
