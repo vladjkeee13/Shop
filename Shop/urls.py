d4831a96c501d48ffd845fcb6e6f4c6fb14816fa
vladjkeee13 <vladjkeee13@ukr.net>
2019-03-06 14:03:01 +0200

    rating for review was added

--- Shop/urls.py
+++ Shop/urls.py
@@ -19,9 +19,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from product.views import RatingView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(("core.urls", "core"), namespace="core")),
+    path('set-rating/', RatingView.as_view())
 ]
 
 
--- product/views.py
+++ product/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from product.models import Comment
+
+
+class RatingView(View):
+
+    def get(self, request):
+
+        review_id = request.GET['review_id']
+        is_increment = int(request.GET['is_increment'])
+
+        if is_increment:
+            Comment.comment_manager.filter(id=review_id).update(rating=F('rating') + 1)
+        else:
+            Comment.comment_manager.filter(id=review_id).update(rating=F('rating') - 1)
+
+        return JsonResponse({'status': 'success'})
+
