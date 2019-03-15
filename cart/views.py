b74f3def74bdcbec69d61beb7c910a72027e5916
vladjkeee13 <vladjkeee13@ukr.net>
2019-03-15 13:48:55 +0200

    change number of items and prices

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import DetailView
@@ -65,3 +68,29 @@ class RemoveItemFromCartView(View):
         cart.remove_from_cart(item)
 
         return redirect('cart:cart')
+
+
+class ChangeItemQuantity(View):
+
+    def get(self, request):
+
+        try:
+            cart_id = self.request.session['cart_id']
+            cart = Cart.objects.get(id=cart_id)
+            self.request.session['total'] = cart.items.count()
+        except:
+            cart = Cart()
+            cart.save()
+            cart_id = cart.id
+            self.request.session['cart_id'] = cart_id
+            cart = Cart.objects.get(id=cart_id)
+
+        qty = request.GET['qty']
+        item_id = request.GET['item_id']
+
+        cart_item = CartItem.objects.get(id=int(item_id))
+        cart_item.quantity = int(qty)
+        cart_item.item_total = int(qty) * Decimal(cart_item.size.product.price)
+        cart_item.save()
+
+        return JsonResponse({'cart_total': cart.items.count(), 'item_total': cart_item.item_total})
